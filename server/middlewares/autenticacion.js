c30dfd1ae203411a29a4478f8e8a963ae1e71b82
Nicolas Cazco <ncazco@est.ups.edu.ec>
2021-01-20 11:52:11 -0500

    Configuraciones

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,13 @@
+//===========================
+//Verificar Token
+//===========================
+
+let verificaToken=(req,res,next)=>{
+
+    let token=req.get('token');
+
+    res.json({
+        token:token
+    });
+    
+}
