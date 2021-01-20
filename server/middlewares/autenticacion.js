195cf2b80a9d7a4c1e00cbbcfc71607dc0a69e8c
Nicolas Cazco <ncazco@est.ups.edu.ec>
2021-01-20 12:10:26 -0500

    Configuraciones

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -1,3 +1,5 @@
+const jwt=require('jsonwebtoken');
+
 //===========================
 //Verificar Token
 //===========================
@@ -6,8 +8,20 @@ let verificaToken=(req,res,next)=>{
 
     let token=req.get('token');
 
-    res.json({
-        token:token
-    });
-    
+    jwt.verify(token,process.env.SEED,(err,decoded)=>{
+        if(err){
+            return res.status(401).json({
+                ok:false,
+                err
+            })
+        }
+
+        req.usuario=decoded.usuario;
+        next();
+    })
+
+}
+
+module.exports={
+    verificaToken
 }
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,9 +3,11 @@ const Usuario = require("../models/usuario");
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
+const {verificaToken}=require('../middlewares/autenticacion')
+
 const app = express();
 
-app.get("/usuario", function (req, res) {
+app.get("/usuario", verificaToken, function (req, res) {
 
   let desde = req.query.desde || 0;
   desde = Number(desde);
